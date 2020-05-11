1f57f354eb2e583aa0f89f639fe1b63ac632f18f
PENGUINLIONG <admin@penguinliong.moe>
2020-05-11 22:53:15 +0800

    Init

---
+++ SoulAccess-Hub/Controllers/ObjectController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SoulAccess.Hub.Controllers {
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ObjectController : ControllerBase {
+        const int MAX_NENTRY = 20;
+        private readonly ObjectIndexer _Idxr;
+
+        public ObjectController(ObjectIndexer idxr) {
+            _Idxr = idxr;
+        }
+
+        public class GetRangeParams {
+            public DateTime? Since { get; set; }
+            public DateTime? Until { get; set; }
+        }
+
+        // GET: api/v1/object
+        [HttpGet]
+        public IEnumerable<ObjectIndex> Get([FromQuery] GetRangeParams rng) {
+            var ie = _Idxr as IEnumerable<ObjectIndex>;
+            if (rng.Since.HasValue) {
+                var since = rng.Since.Value.ToUniversalTime();
+                ie = ie.SkipWhile(x => x.LastModifiedUtc < since);
+            }
+            if (rng.Until.HasValue) {
+                var until = rng.Until.Value.ToUniversalTime();
+                ie = ie.TakeWhile(x => x.LastModifiedUtc <= until);
+            }
+            return ie.Take(MAX_NENTRY);
+        }
+
+        // GET: api/v1/object/5.zip
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<ActionResult> Get(string name) {
+            try {
+                if (!_Idxr.OpenRead(name, out var fs)) {
+                    return BadRequest("object doesn't exists");
+                }
+                Response.ContentType = "application/octet-stream";
+                using (fs) { await fs.CopyToAsync(Response.Body); }
+                return Ok();
+            } catch (Exception) {
+                return BadRequest("transmission failed");
+            }
+        }
+
+        // PUT: api/v1/object/5.zip
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(string name) {
+            try {
+                if (!_Idxr.OpenWrite(name, out var fs)) {
+                    return BadRequest("object already exists");
+                }
+                using (fs) { await Request.BodyReader.CopyToAsync(fs); }
+                return Ok();
+            } catch (Exception) {
+                return BadRequest("transmission failed");
+            }
+        }
+
+        // DELETE: api/v1/object/5.zip
+        [HttpDelete("{id}")]
+        public bool Delete(string name) {
+            return _Idxr.Remove(name);
+        }
+    }
+}
---
+++ SoulAccess-Hub/ObjectIndexer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoulAccess.Hub {
+    public class ObjectIndexerConfig {
+        public string StorageDirPath = "~/storage";
+    }
+
+    public class ObjectIndex {
+        // Name of the file. Also the identifier of the file.
+        public string Name;
+        // The time when the latest modificaiton was done.
+        public DateTime LastModifiedUtc;
+        // The physical size of the file.
+        public long FileSize;
+
+        public ObjectIndex(FileInfo fi) {
+            Name = fi.Name;
+            LastModifiedUtc = fi.LastWriteTimeUtc;
+            FileSize = fi.Length;
+        }
+    }
+
+    public class ObjectIndexer : IEnumerable<ObjectIndex> {
+        // Used to synchronize accesses.
+        private object _SyncRoot;
+        private readonly ObjectIndexerConfig _Cfg;
+        // Index is sorted by the last modification time.
+        private List<ObjectIndex> _Idxs;
+        private Dictionary<string, ObjectIndex> _NameMap;
+        private Comparer<ObjectIndex> _LastModCmp;
+
+        public ObjectIndexer() {
+            _Cfg = new ObjectIndexerConfig();
+            _Idxs = new List<ObjectIndex>();
+            _SyncRoot = new object();
+            _LastModCmp = Comparer<ObjectIndex>.Create((a, b) => {
+                return -a.LastModifiedUtc.CompareTo(b.LastModifiedUtc);
+            });
+            if (!Directory.Exists(_Cfg.StorageDirPath)) {
+                Directory.CreateDirectory(_Cfg.StorageDirPath);
+            }
+        }
+
+        // Get the index of a file identified by `name`.
+        //
+        // Returns false if the file hasn't been indexed.
+        public bool TryGetIndex(string name, out ObjectIndex idx) {
+            return _NameMap.TryGetValue(name, out idx);
+        }
+        // Index all local files in the storage directory. The indexed data will
+        // be kept if the call failed.
+        public bool IndexAll() {
+            lock (_SyncRoot) {
+                IEnumerable<ObjectIndex> idxs;
+                try {
+                    idxs = from x in Directory.EnumerateFiles(_Cfg.StorageDirPath)
+                           select new ObjectIndex(new FileInfo(_Cfg.StorageDirPath + x));
+                } catch (Exception) {
+                    return false;
+                }
+                _Idxs.Clear();
+                _Idxs.AddRange(idxs);
+                _Idxs.Sort(_LastModCmp);
+                _NameMap = _Idxs.ToDictionary(x => x.Name);
+                return true;
+            }
+        }
+        // Attempt to add a file identified by `name` to the object. The file
+        // MUST exists in the file system before being added.
+        //
+        // Returns false if the file doesn't exists.
+        public bool Add(string name) {
+            var path = Path.Combine(_Cfg.StorageDirPath, name);
+            lock (_SyncRoot) {
+                if (!File.Exists(name)) { return false; }
+                FileInfo fi;
+                try {
+                    fi = new FileInfo(path);
+                } catch (Exception) {
+                    return false;
+                }
+                var idx = new ObjectIndex(fi);
+                _Idxs.BinarySearch(idx, _LastModCmp);
+                _NameMap.Add(name, idx);
+                return true;
+            }
+        }
+        // Remove a file from the index, the corresponding physical file will
+        // also be removed from local storage. A file will not be removed if it
+        // it's not indexed.
+        //
+        // Return `false` if the file doesn't exists beforehand; otherwise,
+        // `true` is returned.
+        public bool Remove(string name) {
+            var path = Path.Combine(_Cfg.StorageDirPath, name);
+            lock (_SyncRoot) {
+                if (_NameMap.TryGetValue(name, out var idx)) {
+                    _NameMap.Remove(name);
+                    _Idxs.Remove(idx);
+                    try {
+                        if (File.Exists(name)) { File.Delete(path); }
+                    } catch (Exception) {
+                        // Ignore deletion error.
+                    }
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        // Open a stream to an existing file.
+        //
+        // Returns false if the file doesn't exists.
+        public bool OpenRead(string name, out FileStream fs) {
+            var path = Path.Combine(_Cfg.StorageDirPath, name);
+            lock (_SyncRoot) {
+                if (File.Exists(path)) { goto fail; }
+                try {
+                    fs = new FileStream(path, FileMode.Open, FileAccess.Read,
+                        FileShare.Read, 4096, useAsync: true);
+                    return true;
+                } catch (Exception) { goto fail; }
+            }
+        fail:
+            fs = null;
+            return false;
+        }
+        // Create a stream to a newly created file. The created file WILL NOT be
+        // automatically added to.
+        //
+        // Returns false if the file already exists.
+        public bool OpenWrite(string name, out FileStream fs) {
+            var path = Path.Combine(_Cfg.StorageDirPath, name);
+            lock (_SyncRoot) {
+                if (File.Exists(path)) { goto fail; }
+                try {
+                    fs = new FileStream(path, FileMode.Create, FileAccess.Write,
+                        FileShare.None, 4096, useAsync: true);
+                    return true;
+                } catch (Exception) { goto fail; }
+            }
+        fail:
+            fs = null;
+            return false;
+        }
+
+        public IEnumerator<ObjectIndex> GetEnumerator() {
+            return ((IEnumerable<ObjectIndex>)_Idxs).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return ((IEnumerable<ObjectIndex>)_Idxs).GetEnumerator();
+        }
+    }
+}
